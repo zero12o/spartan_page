dfdccf1cafb5805de2e0fd2ca426e71c071e24a2
zero12o <mobius.zero12o.kero@gmail.com>
2016-09-16 04:39:21 -0400

    Updated process.php - email validation shown false postive.

--- _js/carousel_control_actions.js
+++
@@ -1,9 +0,0 @@
-// ============================================================================================
-//	carousel control actions
-// ============================================================================================
-$(".carousel-control.left").click(function () {
-	$("#testimonial").carousel("prev");
-});
-$(".carousel-control.right").click(function () {
-	$("#testimonial").carousel("next");
-});
